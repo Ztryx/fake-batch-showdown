f2bcb6e487995bd3a16456329df9dc2721ab9c36
Andrés Jesús Díaz Santos <not.andres.diaz@gmail.com>
2020-04-19 16:16:01 +0200

    Add seeder for 20 randomly Users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Str;
 |
 */
 
+define('TIMEZONE', ['CET', 'CST', 'GMT+1']);
+
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
@@ -24,6 +26,6 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-        'timezone' => ''
+        'timezone' => TIMEZONE[rand(0, (count(TIMEZONE) - 1))],
     ];
 });
