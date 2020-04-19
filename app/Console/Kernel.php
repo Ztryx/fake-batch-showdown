3733569aa2391a6e8166938449af18859bfba35f
Andrés Jesús Díaz Santos <not.andres.diaz@gmail.com>
2020-04-19 18:53:07 +0200

    Add Service Feature to provide update when batch has not been completed yet

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\UpdateUsersRandomly::class,
+        Commands\UpdateUserRandomly::class,
+        Commands\ClearBatchCounter::class,
     ];
 
     /**
--- app/Services/FakeBatchAPI.php
+++ app/Services/FakeBatchAPI.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Repositories\User\UserRepositoryInterface;
 use Carbon\Carbon;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
@@ -21,20 +22,33 @@ class FakeBatchAPI
         $last_batch_updated_user = DB::table('last_batch_updated_user')->latest('id')->first()->updated_at;
         $users_updatable_list = $this->userRepository->getAllUpdatable($last_batch_updated_user);
         $requests_number = DB::table('batch_counter')->latest('id')->first()->requests_number;
-        if($users_updatable_list->count() == env('PAYLOAD_SIZE', self::PAYLOAD_SIZE) && $requests_number < env('REQUEST_LIMIT', self::REQUEST_LIMIT)) {
-            $requests_number = $requests_number + 1;
-            Log::info('Batch number: '.$requests_number);
-            DB::table('batch_counter')->latest('id')->update(['requests_number' => $requests_number]);
-            DB::table('last_batch_updated_user')->insert([
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ]);
-            foreach ($users_updatable_list as $updateable_user) {
-                Log::info('['.$updateable_user->id.']'.'firstname: '.$updateable_user->firstname.' '.'timezone: '.$updateable_user->timezone);
-            }
-        }
+        if($users_updatable_list->count() == env('PAYLOAD_SIZE', self::PAYLOAD_SIZE) && $requests_number < env('REQUEST_LIMIT', self::REQUEST_LIMIT))
+            $this->fakeLogUpdatedUsers($requests_number, $users_updatable_list);
         //Fake realtime delay in order to check If is actually printing that messages as how should be
         sleep(1);
+    }
+
+    public function fakeUpdateLeftUsers() {
+        $last_batch_updated_user = DB::table('last_batch_updated_user')->latest('id')->first()->updated_at;
+        $users_updatable_list = $this->userRepository->getAllUpdatable($last_batch_updated_user);
+        $requests_number = DB::table('batch_counter')->latest('id')->first()->requests_number;
+        if($users_updatable_list->count() > 0 && $requests_number < env('REQUEST_LIMIT', self::REQUEST_LIMIT))
+            $this->fakeLogUpdatedUsers($requests_number, $users_updatable_list);
+        //Fake realtime delay in order to check If is actually printing that messages as how should be
+        sleep(1);
+    }
+
+    private function fakeLogUpdatedUsers(int $requests_number, Collection $users_updatable_list) {
+        $requests_number = $requests_number + 1;
+        Log::info('Batch number: '.$requests_number);
+        DB::table('batch_counter')->latest('id')->update(['requests_number' => $requests_number]);
+        DB::table('last_batch_updated_user')->insert([
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        foreach ($users_updatable_list as $updateable_user) {
+            Log::info('['.$updateable_user->id.']'.'firstname: '.$updateable_user->firstname.' '.'timezone: '.$updateable_user->timezone);
+        }
     }
 
 }
