1b1d939bec917c5c872c0c00c831f824a3918ddc
Andrés Jesús Díaz Santos <not.andres.diaz@gmail.com>
2020-04-19 17:44:08 +0200

    Add UserSeeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, (int) env('USER_SEEDER_NUMBER', 20))->create();
+    }
+}
